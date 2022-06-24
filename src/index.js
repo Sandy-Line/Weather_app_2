b0b160aa9de6102dcd292ea39ab3df3dcdc52861
Sandy <phbloomwood@gmail.com>
2022-06-24 11:20:21 -0400

    Updated the API url
    When writing the city directly in the url, no need for { }

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+function displayTemp(response) {
+  console.log(response);
+}
+
 let apiKey = "59ebc73950183d72b027190e832e1b5b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={Seoul}&appid={apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
