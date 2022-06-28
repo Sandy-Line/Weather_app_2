edee3b3896e834b47ff97320e5120136f9b06af1
Sandy <phbloomwood@gmail.com>
2022-06-28 11:10:46 -0400

    deleted function changeImg

--- src/index.js
+++ src/index.js
@@ -21,7 +21,26 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function changeImg(iconUrl) {
+function displayTemp(response) {
+  console.log(response);
+  let city = document.querySelector("h1");
+  city.innerHTML = response.data.name;
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = response.data.weather["0"].main;
+  let temp = document.querySelector("#temperature");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = Math.round(response.data.main.humidity);
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let image = document.querySelector("#image");
+  let icon = response.data.weather["0"].icon;
+  let iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  image.setAttribute("src", iconUrl);
+
   if (iconUrl === `http://openweathermap.org/img/wn/01d@2x.png`) {
     document.getElementById("image").src = "medias/sun.png";
   } else if (iconUrl === `http://openweathermap.org/img/wn/02d@2x.png`) {
@@ -46,28 +65,7 @@ function changeImg(iconUrl) {
   }
 }
 
-function displayTemp(response) {
-  console.log(response);
-  let city = document.querySelector("h1");
-  city.innerHTML = response.data.name;
-  let date = document.querySelector("#date");
-  date.innerHTML = formatDate(response.data.dt * 1000);
-  let description = document.querySelector("#weather-description");
-  description.innerHTML = response.data.weather["0"].main;
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = Math.round(response.data.main.temp);
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = Math.round(response.data.main.humidity);
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = Math.round(response.data.wind.speed);
-  let image = document.querySelector("#image");
-  let icon = response.data.weather["0"].icon;
-  let iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-  image.setAttribute("src", changeImg(iconUrl));
-}
-
 let apiKey = "59ebc73950183d72b027190e832e1b5b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
